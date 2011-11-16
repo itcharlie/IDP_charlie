ba317fd2c4182f42d6ef78dd708695d6f1a0a88c
Charlie Gonzalez <Charlie Gonzalez>
2011-11-16 00:42:38 -0500

    A shorter more efficient euler_1 script.

--- prj_euler/euler_1.pl
+++ prj_euler/euler_1.pl
@@ -1,13 +1,33 @@
 use strict;
 use Data::Dumper;
 
-# Euler problem :
-#
+print"
+# 
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we 
 # get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the 
 # multiples of 3 or 5 below 1000.
-#
+# \n\n";
 
+
+my $sum = '0';
+
+for my $natural ( 1 ... 999 ) {
+   if ($natural % 3 == 0 ) {
+      $sum += $natural;
+   } elsif ( $natural =~ /0$|5$/ ) {
+      $sum += $natural;
+   };
+   
+}
+
+print "The total of the multiples of 3 and 5 is: " . $sum . "\n\n";
+
+
+
+
+
+# this is the old way I solved this problem.
+__END__
 my @list;
 my $sum; 
 
