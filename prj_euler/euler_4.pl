2c835c0c1cdc93961c4bbb64482673a96b6e4aa2
Charlie Gonzalez <itcharlie@gmail.com>
2011-11-17 16:38:42 -0500

    Adding new changes
    Problem # 4

---
+++ prj_euler/euler_4.pl
@@ -0,0 +1,11 @@
+use strict;
+use Data::Dumper;
+
+#
+#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+#Find the largest palindrome made from the product of two 3-digit numbers.
+
+# I will first use the example to detemine how the numbers are found.
+
+my @list_two = ( 10 ... 99 );
+
