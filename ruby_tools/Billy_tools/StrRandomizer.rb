f55ad5621571a5fbab0a144fa5b83be1e54eb205
Charlie Gonzalez <itcharlie@gmail.com>
2012-09-26 18:49:32 -0400

    address method

--- ruby_tools/Billy_tools/StrRandomizer.rb
+++ ruby_tools/Billy_tools/StrRandomizer.rb
@@ -6,10 +6,23 @@ class StrRandomizer
     @fnames = [ "Patrick", "Geno", "Sam","Alfred","Mario","Luigi","Rocky","Joe" ]
     @lnames = [ "Gonzalez", "Sanchez", "Neira","Santos","Ramirez","Reyes" ]
     @comp_types = [ "Grocery Store", "Tatto Shop", "Auto Parts", "Pharmacy", "Burger Shop"]
-    
+     
     @comp_type = @comp_types[rand(@comp_types.size)]
     @fname = @fnames[rand(@fnames.size)]
     @lname = @lnames[rand(@lnames.size)]
+
+    @street_num = []
+    rand(1..4).times do 
+        num = rand(1..9)
+        @street_num.push(num) 
+    end 
+    
+    @street_names = [ "Doe", "Gold", "Fulton", "Main", "Bedford" ] 
+    @street_types = [ "Avenue", "Street", "Road", "Boulevard" ]
+    
+    @street_name = @street_names[rand(@street_names.size)]
+    @street_type  = @street_types[rand(@street_types.size)]
+
   end
 
   def company_name
@@ -28,6 +41,8 @@ class StrRandomizer
     @lname
   end
 
-
+  def address
+     @street_num.join + " " + @street_name + " " + @street_type 
+  end
 
 end
--- ruby_tools/Billy_tools/create_client.rb
+++ ruby_tools/Billy_tools/create_client.rb
@@ -4,13 +4,13 @@ require_relative 'StrRandomizer.rb'
 
 
 99.times do
-  sr= StrRandomizer.new
- 
+  sr= StrRandomizer.new 
+
   postData = Net::HTTP.post_form(URI.parse('http://localhost:3000/clients/create'),
 			{'company_name'=> sr.company_name,
 			'contact_fname'=> sr.fname,
 			'contact_lname'=> sr.lname,
-			'address_1'=>'123 Doe St',
+			'address_1'=> sr.address,
 			'address_2'=>'',
 			'city'=>'NY',
 			'state'=>'NY',
