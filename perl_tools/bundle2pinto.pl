bd4f98485bd56e5ed1f019a18eaa2068352d930d
Charlie Gonzalez <itcharlie@gmail.com>
2014-01-01 23:54:58 -0500

    make it work before refactoring to make it run faster.

--- perl_tools/bundle2pinto.pl
+++ perl_tools/bundle2pinto.pl
@@ -19,6 +19,12 @@ my %modules =();
 my %undef_versions = ();
 my $head_cont = 0;
 
+my $REPO = "bundle_" . time();
+
+# Create a Pinto repo and pass in the ur
+system( "pinto -r ~/" . $REPO . " init") ==0 
+	or die "Unable to create pinto repo : $REPO $!";
+
 while ( my $line  = <$fh>) {
 	
 	if ( $line =~ /^\=head1\sCONTENTS/ ) {
@@ -44,16 +50,47 @@ while ( my $line  = <$fh>) {
 }
 
 my %dist_archives =();
-for my $mod ( keys %modules ) {
+
+my $count =0;
+for my $mod  ( keys %modules ) {
+	last if $count > 10;
 	# Store the archive url in the hash for the modules that do have versions defined
 	my $archive_url = dist_archive_url( $mod, $modules{$mod}{'VERSION'} ) ;
 	next if ( ! $archive_url );
 	$dist_archives{$archive_url} = 1;
+	$count++;
 }
 
-print Dumper \%dist_archives;
+#print Dumper \%dist_archives;
 #print Dumper \%undef_versions;
 
+mkdir($ENV{HOME} . "/cpan_archive") if ( ! -d $ENV{HOME} . "/cpan_archive");
+chdir($ENV{HOME} . "/cpan_archive");
+
+for my $archive_url ( keys %dist_archives ) {
+	my @archive_string = split( /\// , $archive_url );
+	my $archive_file = $archive_string[-1];
+	if ( -f $archive_file ) {
+		add_to_repo($archive_file);
+		next;
+	}
+	my $cmd = "wget " . $archive_url;
+	system( $cmd ) == 0 
+		or die "Unable to download file $archive_url  $!\n";
+	
+	add_to_repo($archive_file);
+}
+
+sub add_to_repo {
+	my $archive_file = shift;
+	
+	# Need to figure out a way to auto commit with default message		
+	my $cmd = "pinto -r ~/" . $REPO . " add " . $ENV{HOME} . "/cpan_archive/" . $archive_file;
+	
+	system( $cmd ) == 0 
+		or die "Unable to add $archive_file to repo $!";
+	return 1;
+}
 
 # Attempt to search for Module archive via cpan api.
 sub dist_archive_url {
@@ -75,7 +112,11 @@ sub dist_archive_url {
 	my $archive_url;	
 	for my $release  ( @{$dist_data->{releases}} ) {
 		if ( $release->{version} eq $version ) {
-			$archive_url =  $release->{cpanid} . "/" . $release->{'archive'};
+			$archive_url =  "http://search.cpan.org/CPAN/authors/id/"
+			. substr($release->{cpanid},0,1) . "/" 
+			. substr($release->{cpanid},0,2) . "/" 
+			. $release->{cpanid} . "/"
+			. $release->{'archive'};
 		}
 	}
  	return $archive_url;
@@ -83,4 +124,3 @@ sub dist_archive_url {
 
 
 
-# Create a Pinto repo and pass in the ur
