1a77b05cffa600290b6594a1c0dbeb8b7e29fb3c
Charlie Gonzalez <itcharlie@gmail.com>
2016-03-10 11:20:28 -0500

    generic Rails install script for Ubuntu
    Bare minimum script that installs Rails on Ubuntu.

---
+++ ruby_tools/ubuntu_rails_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt-get -y update && sudo apt-get -y upgrade 
+sudo apt-get install -y  build-essential git libssl-dev libreadline-dev zlib1g-dev sqlite3 libsqlite3-dev
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+
+cd ~/.rbenv && src/configure && make -C src
+cd ~
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"'  >> ~/.bashrc
+
+source ~/.bashrc
+
+rbenv install 2.3.0
+
+rbenv rehash
+
+rbenv global 2.3.0
+
+gem install rails
